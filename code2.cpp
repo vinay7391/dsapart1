18eb39fa7b81401b13a9ab166d700b40a85813bb
vinay7391 <thakurvinay7391@gmail.com>
2024-03-16 11:31:44 +0530

    second code

---
+++ code2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+ int main(){
+int a;
+cout<<"enter the value of a:"<<endl;
+cin>>a;
+if(a>0){
+    cout<<"a is positive"<<endl;
+
+}
+else{
+if(a<0)
+{
+
+    cout<<"a is negative:"<<endl;
+
+}
+else{
+    cout<<"a is 0:"<<endl;
+}
+}
+
+
+    return(0);
+ }
