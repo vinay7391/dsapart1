bbb05257d59aaa1b712abae51eca8f9be74b196c
vinay7391 <thakurvinay7391@gmail.com>
2024-03-16 11:08:38 +0530

    this is verification of no. whether it is a positive no. or negative

---
+++ code1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a = 5;
+if(a>0){
+    cout<<"a is positive "<<endl;
+
+}
+else{
+
+    cout<<"a is negative"<<endl;
+}
+
+
+    return(0);
+}
