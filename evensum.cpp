1e84e8a251abcf9e8cc51fdb1bf047e902bd584d
vinay7391 <thakurvinay7391@gmail.com>
2024-03-17 15:09:54 +0530

    sum of n even numbers

---
+++ evensum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int i=2;
+int sum=0;
+while (i<=n)
+{
+sum = sum +i;
+i=i+2;
+}
+cout<<"the sum of even numbers:"<<sum;
+    return 0; 
+}
