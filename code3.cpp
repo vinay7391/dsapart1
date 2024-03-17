1ffc60cdaaba7ef1e232fe8cf4d6d3021eb37177
vinay7391 <thakurvinay7391@gmail.com>
2024-03-17 11:51:20 +0530

    to check whether its uppercase or lowercase

---
+++ code3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int  main(){
+char ch;
+cin>>ch;
+if(ch >='a'&&ch <='z'){
+    cout<<"this s lower case"<<endl;
+}
+else if(ch>='A' &&ch<='Z')
+{
+cout<<"this is uppercase:"<<endl;
+}
+
+
+
+
+
+
+    return 0;
+}
