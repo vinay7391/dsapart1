7210722cbb4ed2c415561f433b16e2c8cb199a26
vinay7391 <thakurvinay7391@gmail.com>
2024-03-18 11:17:32 +0530

    pattern9

--- pattern9.cpp
+++ pattern9.cpp
@@ -3,13 +3,13 @@
  int main(){
 int n;
 cin>>n;
-int i=1;
-while(i<=n){
-int j=1;
-int value=i;
-while(j<=i){
-    cout<<value<<" ";
-    value =value+1;
+int i=0;
+while(i<n){
+int j=0;
+
+while(j<i){
+    cout<<i+j;
+    
     j=j+1;
 
 }
