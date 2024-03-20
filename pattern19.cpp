2e2f0be09abce960f7720da168092660c91980bd
vinay7391 <thakurvinay7391@gmail.com>
2024-03-20 18:51:22 +0530

    pattern19

---
+++ pattern19.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n ;
+cin>>n;
+int i =1;
+while (i<=n){
+    int star = n- i +1;
+    while(star){
+        cout<<"*";
+        star = star -1;
+
+    }
+    int j =1;
+    while(j<=i){
+        cout<<" ";
+        j = j +1;
+        }
+        cout<<endl;
+        i=i+1;
+
+
+}
+return 0;
+}
