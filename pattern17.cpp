8f3d7413905dadc79cb851f0302736e417209d98
vinay7391 <thakurvinay7391@gmail.com>
2024-03-20 17:53:52 +0530

    pattern17

---
+++ pattern17.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n ;
+cin>>n;
+int i =1;
+while(i<=n){
+    int j = 1;
+    char start = 'A' + i - 1;
+    while(j<=n){
+        
+        cout<<start;
+        start = start +1;
+        j = j +1;
+    }
+    cout<<endl;
+    i = i +1;
+}
+return 0;
+}
